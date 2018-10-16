60121f2a7cefcb80296e67439a3980d3ea679462
Karsten Mikulcak <karsten.mikulcak@gmail.com>
2018-10-16 17:51:51 +0200

    optimized routing

--- _app/components/infrastructure/router.js
+++ _app/components/infrastructure/router.js
@@ -4,47 +4,27 @@ import {Dashboard} from "../modules/monitoring/dashboard";
 import {ListView} from "../modules/monitoring/listView";
 import {Detail} from "../modules/monitoring/detail";
 
-
-const Monitoring = (props)=>{
-    console.log(props);
-    return(
-        <div>
-            <Route exact path={`${props.match.path}/`} component={Dashboard}/>
-            <Route path={`${props.match.path}/dashboard/`} component={Dashboard}/>
-            <Route path={`${props.match.path}/listView/:state?/:count?`} component={ListView}/>
-            <Route path={`${props.match.path}/detail/:id`} component={Detail}/>
-        </div>
-    );
-}
-/*
-*          <Route
-                path='/monitoring/detail/:id'
-                render={(props) => <div>Detail: {props.match.param.id}</div>}
-            />
-            <Route
-                path='/monitoring/listView(/:state)(/:count)'
-                render={(props) => <div>ListView: {props.match.param.state} | {props.match.param.count}</div>}
-            />
-            <Route
-                path='/monitoring/dashboard'
-                component={Dashboard}
-            />
-
-*
-* */
-
 export const AppRouter = (props) => {
     return (
-        <BrowserRouter
-            basename='/'
-        >
+        <BrowserRouter>
             <Switch>
                 <Route
                     path='/monitoring'
-                    component={Monitoring}>
-
-                </Route>
-
+                    component={Dashboard}
+                    exact={true}
+                />
+                <Route
+                    path='/monitoring/listView/:state?/:count?/detail/:id'
+                    component={Detail}
+                />
+                <Route
+                    path='/monitoring/listView/:state?/:count?'
+                    component={ListView}
+                />
+                <Route
+                    path='/monitoring/dashboard'
+                    component={Dashboard}
+                />
                 <Route
                     path='/mdlt'
                     render={() => <div>mdlt</div>}
@@ -54,7 +34,6 @@ export const AppRouter = (props) => {
                     render={() => <div>ptopt</div>}
                 />
             </Switch>
-
         </BrowserRouter>
     );
 }
